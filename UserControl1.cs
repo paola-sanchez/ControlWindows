502bf7e919d22f49eb7d6dfe9cbbe54f178c8401
Paosa20 <msanchezc1@ucenfotec.ac.cr>
2020-09-22 18:10:19 -0600

    Add project files.

---
+++ UserControl1.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ControlWindows
+{
+    public partial class UserControl1: UserControl
+    {
+        public UserControl1()
+        {
+            InitializeComponent();
+        }
+
+        private void UserControl1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            this.textBox1.Text = "Hola Mundo";
+
+        }
+    }
+}
